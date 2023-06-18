55036c8a8ffcd50e2af9bf0b579af6cf0fca4000
Pooja Kumari <35136890+poojakumari11228@users.noreply.github.com>
2023-06-17 19:54:29 -0500

    LinkedList Implementation

---
+++ LinkedList/JavaLinkedList.java
@@ -0,0 +1,15 @@
+package DataStructure.LinkedList;
+
+import java.util.LinkedList;
+
+public class JavaLinkedList {
+
+	
+	
+	public static void main(String[] args) {
+
+		LinkedList<String> l = new LinkedList<>();
+		l.add("Pooja ");
+		
+	}
+}
